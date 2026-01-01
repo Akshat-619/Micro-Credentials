719404e2919e49032602e7770d8154602d7a32d9
Akshat Sharma <Akshat.Sharma@ad.mastersofterp.com>
2026-01-01 19:14:07 +0530

    Updated website

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
-
-
-  
+document.getElementById("sign-in").addEventListener("click", () => {
+  document.getElementById("auth-buttons").classList.add("d-none");
+  document.getElementById("user-icons").classList.remove("d-none");
+});
